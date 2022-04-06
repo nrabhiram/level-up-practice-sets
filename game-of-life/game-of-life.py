e0d01c065289273ab8076f6be2c321ef522f44b6
Abhiram Reddy <nrabhiramr@gmail.com>
2022-04-07 00:50:15 +0530

    rules

---
+++ game-of-life/game-of-life.py
@@ -0,0 +1,5 @@
+#Rules of the game:
+#Consider a single cell 
+#   1. If cell is alive and number of neighbours alive is 2 or 3, cell continues to remain alive in the next iteration
+#   2. If cell is dead and exactly 3 neighbours are alive, cell is revived in the next iteration
+#   3. Remaining cells that are alive will die (and dead cells will remain dead) in the next iteration
