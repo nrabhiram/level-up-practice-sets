6bab5826e3a16409b2d36b3ec6b54b74944e8a43
Abhiram Reddy <nrabhiramr@gmail.com>
2022-04-07 01:36:12 +0530

    procedure ideation 3

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -12,6 +12,8 @@
 #           * In this method, we have to locate the 8 neighbours surrounding a specific cell
 #           ? How to locate? This is a 2D grid, so there'll be an X coordinate, and a Y coordinate. We could assume that the first cell is located at (0, 0).
 #           ! There must be a formula to find the 8 neighbours that applies for every integer ranging from 1...n; where n = rows * columns, i.e. the final cell
+#           ! View formula-1.jpg. I think it checks out for all square grids. Will check if it replicates for other values of n by induction. Next, have to figure out edge cases, i.e. how to check if a neighbour exists in that location in the first place?
+#           ? What if the number of grids and columns aren't equal?
 #           * Once located, we have to determine whether the cell should be dead or alive in the next iteration
 #           ? How do we configure the initial state, when the game first starts. 
 #           ? Do we assume that all of the cells are dead/alive? 
