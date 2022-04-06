54de6a7a75250019cf0bb6227d6f8a93c6983e63
Abhiram Reddy <nrabhiramr@gmail.com>
2022-04-07 02:02:20 +0530

    procedure ideation 5

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -10,10 +10,17 @@
 #       - Number of columns
 #       - Method to set the initial configuration of the cells and the configuration after every iteration
 #           * In this method, we have to locate the 8 neighbours surrounding a specific cell
-#           ? How to locate? This is a 2D grid, so there'll be an X coordinate, and a Y coordinate. We could assume that the first cell is located at (0, 0).
-#           ! There must be a formula to find the 8 neighbours that applies for every integer ranging from 1...n; where n = rows * columns, i.e. the final cell
-#           ! View formula-1.jpg. I think it checks out for all square grids. Will check if it replicates for other values of n by induction. Next, have to figure out edge cases, i.e. how to check if a neighbour exists in that location in the first place?
-#           ? What if the number of grids and columns aren't equal?
+#           ! Formula to locate a cell's 8 neighbours (assuming there are l columns and h rows, and id of cell is n):
+#               - T: n - l
+#               - B: n + l
+#               - L: n - 1
+#               - R: n + 1
+#               - TR: n - (l - 1)
+#               - TL: n - (l + 1)
+#               - BR: n + (l + 1)
+#               - BL: n + (l - 1)
+#               - Edge cases: check if the id of the neighbour is >= 0 and <= l * h - 1 (in order to see if it actually exists; corner cells might miss some neighbours)
+#               - View formula-1.jpg and formula-2.jpg to see how I arrived at the formula
 #           * Once located, we have to determine whether the cell should be dead or alive in the next iteration
 #           ? How do we configure the initial state, when the game first starts. 
 #           ? Do we assume that all of the cells are dead/alive? 
